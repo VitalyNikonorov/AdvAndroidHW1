07fa2ebe747a3087e585ef89044c699b3e69c51a
Виталий Никоноров <vitaly@MacBook-Pro-Vitalij.local>
2015-10-08 23:52:20 +0300

    splashscreen activity

---
+++ app/src/main/java/net/nikonorov/advandroidhw1/ActivitySplash.java
@@ -0,0 +1,15 @@
+package net.nikonorov.advandroidhw1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by vitaly on 09.10.15.
+ */
+public class ActivitySplash extends Activity {
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}

73d1e15c84221fa72b0a2419e0c4b878b8d37f34
Виталий Никоноров <vitaly@MacBook-Pro-Vitalij.local>
2015-10-09 10:19:20 +0300

    add listactivity, but needs to improve sleep

---
+++ app/src/main/java/net/nikonorov/advandroidhw1/ActivityList.java
@@ -0,0 +1,15 @@
+package net.nikonorov.advandroidhw1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by vitaly on 09.10.15.
+ */
+public class ActivityList extends Activity {
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
--- app/src/main/java/net/nikonorov/advandroidhw1/ActivitySplash.java
+++ app/src/main/java/net/nikonorov/advandroidhw1/ActivitySplash.java
@@ -1,6 +1,7 @@
 package net.nikonorov.advandroidhw1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 /**
@@ -11,5 +12,12 @@ public class ActivitySplash extends Activity {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Intent intent = new Intent(this, ActivityList.class);
+        startActivity(intent);
     }
 }

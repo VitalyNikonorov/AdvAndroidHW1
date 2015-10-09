624d87ea9ea5a50a2078004a481e3fe82fbaa775
Виталий Никоноров <vitaly@MacBook-Pro-Vitalij.local>
2015-10-09 19:58:57 +0300

    add icons

--- app/src/main/java/net/nikonorov/advandroidhw1/ActivityList.java
+++ app/src/main/java/net/nikonorov/advandroidhw1/ActivityList.java
@@ -1,15 +1,16 @@
 package net.nikonorov.advandroidhw1;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by vitaly on 09.10.15.
  */
-public class ActivityList extends Activity {
+public class ActivityList extends AppCompatActivity {
 
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        setTitle(R.string.titleListActivity);
     }
 }

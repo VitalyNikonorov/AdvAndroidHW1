2ac5f3c92792530f6c8dec63ffb383329aaf9bef
Виталий Никоноров <vitaly@MacBook-Pro-Vitalij.local>
2015-10-09 11:11:07 +0300

    fix splash sleep

--- app/src/main/java/net/nikonorov/advandroidhw1/ActivitySplash.java
+++ app/src/main/java/net/nikonorov/advandroidhw1/ActivitySplash.java
@@ -8,16 +8,27 @@ import android.os.Bundle;
  * Created by vitaly on 09.10.15.
  */
 public class ActivitySplash extends Activity {
+    private final int PAUSE = 2000;
 
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        
+        Launcher launcher = new Launcher();
+        launcher.start();
+
+    }
+
+    private class Launcher extends Thread{
+
+        public void run(){
+            try {
+                Thread.sleep(PAUSE);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Intent intent = new Intent(ActivitySplash.this, ActivityList.class);
+            startActivity(intent);
         }
-        Intent intent = new Intent(this, ActivityList.class);
-        startActivity(intent);
     }
 }
